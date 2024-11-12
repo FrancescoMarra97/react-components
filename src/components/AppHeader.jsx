d12e06d8505c27dac014ccdbb958da7cd49d44d0
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-12 15:49:49 +0100

    appheader ,appMain, appfooter

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+export default function AppHeader() {
+
+
+    return (
+        <header>
+            <h2>Il Mio Blog</h2>
+
+        </header>
+    )
+}
---
+++ src/components/appMain.jsx
@@ -0,0 +1,17 @@
+export default function AppMain() {
+
+    return (
+        <main>
+
+
+            <div className="container">
+                <div className="blog-card">
+                    <img src="https://via.placeholder.com/600x400" alt="Testo alternativo" />
+                    <h4>Titolo del post</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione ipsam commodi unde iste numquam nemo, sint reiciendis inventore sit eaque.</p>
+                    <button>leggi di piu</button>
+                </div>
+            </div>
+        </main>
+    )
+}
