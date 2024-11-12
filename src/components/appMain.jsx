de99aef4aafcfa2ce943092d6b9ea40a8ad13f2b
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-12 16:40:04 +0100

    css card

--- src/components/appMain.jsx
+++ src/components/appMain.jsx
@@ -2,14 +2,16 @@ export default function AppMain() {
 
     return (
         <main>
-
-
             <div className="container">
-                <div className="blog-card">
-                    <img src="https://via.placeholder.com/600x400" alt="Testo alternativo" />
-                    <h4>Titolo del post</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione ipsam commodi unde iste numquam nemo, sint reiciendis inventore sit eaque.</p>
-                    <button>leggi di piu</button>
+                <div class="card">
+                    <img src="https://via.placeholder.com/600x400" alt="Image" class="card-img-top" />
+                    <div class="card-body">
+                        <h5 class="card-title">TItolo del post</h5>
+                        <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus saepe, aperiam ex asperiores dolorum ipsa accusantium, sed rem sapiente esse veniam reiciendis facilis sint nihil neque! Quas illo corrupti quia!</p>
+                    </div>
+                    <div class="card-footer">
+                        <button>Leggi di più</button>
+                    </div>
                 </div>
             </div>
         </main>
